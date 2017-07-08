f823f7d4ada7beb7dfce858df99f26fc3d0a1e26
Allie <alliemkelley@gmail.com>
2017-07-08 16:23:23 -0700

    Adding carousel

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET friends page. */
 router.get('/', function(req, res, next) {
     //res.send('respond with a resource');
-    res.render('friends', {name: "Peter"});
+    res.render('portfolio', {name: "Peter"});
 });
 
 module.exports = router;
